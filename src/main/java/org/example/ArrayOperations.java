fba8f738b09acd060675f3a6aab744463838810b
Leonardo Lanni <leonardo.lanni@e2ycommerce.com>
2024-07-13 07:56:43 +0100

    add more basics

--- src/main/java/org/example/ArrayOperations.java
+++ src/main/java/org/example/ArrayOperations.java
@@ -2,10 +2,23 @@ package org.example;
 
 public class ArrayOperations {
 
-  //METHOD: sumElements(int[] elements)
+  //1. METHOD: sumElements(int[] elements)
   //requirements
   //empty array returns -1
   //only allow positive integer elements (throws Exception otherwise)
   //return the sum of numbers in any other case
+
+
+  //2. METHOD: findMax(int[] elements)
+  //requirements
+  //empty array returns -1
+  //only allow positive integer elements (throws Exception otherwise)
+  //return the value of the biggest element in the array in any other case
+
+  //3. METHOD: findMaxPosition(int[] elements)
+  //requirements
+  //empty array returns -1
+  //only allow positive integer elements (throws Exception otherwise)
+  //return the index (position in the array, starting from 0) of the biggest element in the array in any other case
 
 }
--- src/main/java/org/example/DataProvider.java
+++ src/main/java/org/example/DataProvider.java
@@ -3,5 +3,5 @@ package org.example;
 import java.util.Random;
 
 public class DataProvider {
-
+  //a class to simulate the retrieval of existing arrays for instance from a persistence layer (DB)
 }
